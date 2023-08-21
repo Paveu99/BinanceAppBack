b25359d43b6377947e65e10d82c68c6112c8ec15
Paweł Jarecki <paweljarecki10@gmail.com>
2023-08-21 10:37:53 +0200

    Trade router updated with new type added to represent a POST to add expression as a favourite to database

--- routers/trade.ts
+++ routers/trade.ts
@@ -1,19 +1,32 @@
 import Router from 'express'
-import {UserRecord} from "../records/user.record";
 import {getData} from "../utils/db2";
+import {TradeRecord} from "../records/trade.record";
+import {AddNewFavourite} from "../types/trade/trade";
+import {ValidationError} from "../utils/errors";
 
-export const tradesRouter = Router()
+export const tradesRouter = Router();
 
 tradesRouter
     .get('/', async (req, res) => {
-        const data = await getData()
-        res.json(data)
+        const data = await getData();
+        res.json(data);
     })
 
     .post('/', async (req, res) => {
+        const newFavourite = new TradeRecord(req.body as AddNewFavourite);
+        await newFavourite.insert();
 
+        res.json(newFavourite);
     })
 
-    .delete('/', async (req, res) => {
+    .delete('/:id', async (req, res) => {
+        const trade = await TradeRecord.getOne(req.params.id);
 
+        if(!trade) {
+            throw new ValidationError('No such trade!');
+        }
+
+        await trade.delete();
+
+        res.end();
     })
